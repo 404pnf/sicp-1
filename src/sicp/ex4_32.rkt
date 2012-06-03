ef9bb63c4d3fa21f9964c3a5e612bfc519406744
Ramakrishnan Muthukrishnan <vu3rdd@gmail.com>
2012-06-04 03:34:15 +0530

    solutions to 4.32 and 4.34

---
+++ src/sicp/ex4_32.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+#|
+
+Here not only the cdr but the car is also lazy. One can represent lazy trees with
+such a cons cell.
+
+|#
---
+++ src/sicp/ex4_34.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+#|
+
+Too 'lazy' to implement this problem.
+
+One can create a type for lazy cons cells and when it comes to printing, one can print 
+until some pre-defined number of cells exhausted and then print "... etc" or something like that.
+
+|#
